479ef7d5f4b7012edc1765a7f4a546655eae9b97
DroidStudioAS <as.droiddevteam@gmail.com>
2024-02-13 14:45:25 +0100

    inicijalni komit

---
+++ templates/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prijava</title>
+
+    <link rel="stylesheet" href="../public/styles.css">
+</head>
+<body>
+    <?php include 'navigacija.php'?>
+    <div class="login_container">
+
+    </div>
+</body>
+
+</html>
---
+++ templates/navigacija.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prijava</title>
+
+    <link rel="stylesheet" href="../public/styles.css">
+</head>
+
+<body>
+    <div class="nav">
+        <img src="../public/resursi/paragraf_logo.png">
+        <div>
+            opcija 1
+        </div>
+        <div>
+            opcija 2
+        </div>
+    </div>
+</body>
+
+</html>
