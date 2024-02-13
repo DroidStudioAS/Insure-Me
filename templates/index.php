7292796f937fb0eea1f665211b89c52c5a426c3e
DroidStudioAS <as.droiddevteam@gmail.com>
2024-02-13 18:58:13 +0100

    dodao inicijalnu formu za unos polise

--- templates/index.php
+++ templates/index.php
@@ -7,6 +7,10 @@
     <title>Prijava Polise</title>
     <link rel="stylesheet" href="../public/styles.css">
     <link rel="icon" href="../public/resursi/paragraf_logo.png"/>
+    <!-- Bootstrap CSS -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap Datepicker CSS -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <body>
@@ -32,7 +36,38 @@
         </h3>
         <div class="input_ime_prezime">
             <input type="name" placeholder="Ime">
-            <input type="na,e" placeholder="Prezime">
+            <input type="name" placeholder="Prezime">
+        </div>
+        <h3>
+            Datum Rodjenja
+        </h3>
+        <input type="date">
+        <h3>
+            Broj Pasosa
+        </h3>
+        <input type="number">
+        <h3>
+            Kontakt
+        </h3>
+        <div>
+            <input placeholder="email" type="email">
+            <input placeholder="Broj Telefona" type="number">
+        </div>
+        <h3>
+            Kad Putujete?
+        </h3>
+        <div class="input-group">
+            <input type="text" class="form-control" id="dateRange">
+            <div class="input-group-append">
+                <span class="input-group-text"><i class="fa fa-calendar"></i></span>
+            </div>
+        </div>
+        <h3>Grupno Ili Individualno Osiguranje</h3>
+        <div class="radio_grupa">
+            <label for="individualno"> Individualno </label>
+            <input id="individualno" type="radio" name="tip_polise">
+            <label for="grupno"> Grupno </label>
+            <input id="grupno" type="radio" name="tip_polise">
 
         </div>
        </form>
@@ -48,6 +83,20 @@
                         console.log('obradi logovanje')
                         $('#loginContainer').toggle()
                         $('#koren').toggle()
+                        $('#dateRange').datepicker({
+                            format: 'yyyy-mm-dd', // Set the desired date format
+                            clearBtn: true, // Show a "Clear" button
+                            todayHighlight: true, // Highlight today's date
+                            multidate: 2, // Allow selection of multiple dates
+                            multidateSeparator: ' - ' // Separator for the selected date range
+                        }).on('changeDate', function(e){
+                              if(e.dates.length >= 2){
+                                 $(this).datepicker('hide');
+                            }
+                            if(e.dates.length > 2){
+                              $(this).datepicker('setDates', [e.date]);
+                             }
+                            });
 
 
                     })
@@ -80,6 +129,10 @@
             });
         });
     </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
+    
+    <!-- Bootstrap Datepicker JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
 </body>
 
 </html>
