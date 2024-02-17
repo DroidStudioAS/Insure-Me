c65bd5aaa878043f725590affe2fa6bf69c5fb49
DroidStudioAS <as.droiddevteam@gmail.com>
2024-02-17 15:31:43 +0100

    dodao odgovarajuce komentare za bolju citljivost na pregled_prijava.php

--- templates/pregled_prijava.php
+++ templates/pregled_prijava.php
@@ -17,23 +17,29 @@
 <body style="background-color:#f4f4f4">
     <?php include 'navigacija.php'?>
     
+    <!--Container za listu prijava i tabelu za prikaz polisa-->
+    <!--Lista prijava-->
     <div class="pregled_koren">
         <div id="listaPrijava" class="list_prijava">
     </div>
-        <div style="display: none;" id="prijave" class="prikaz_prijava">
-            <div id="ime_container" class="prikaz_polje">Ime</div>
-            <div id="rodjendan_container" class="prikaz_polje">rodjen</div>
-            <div id="br_pasosa_container" class="prikaz_polje">br pasosa</div>
-            <div id="email_container" class="prikaz_polje">email</div>
-            <div id="telefon_container" class="prikaz_polje"></div>
-            <div id="od_container" class="prikaz_polje">od</div>
-            <div id="do_container" class="prikaz_polje">do</div>
-            <div id="br_dana_container" class="prikaz_polje">br dana</div>
-            <div id="tip_container" class="prikaz_polje">tip</div>
-           
-            <div id="prikazi_container" class="prikazi_container">prikazi vise</div>
-        </div>      
+     <!--Tabelarni prikaz polise koji se dinamicki popunjava i prikazuje-->
+    <div style="display: none;" id="prijave" class="prikaz_prijava">
+        <div id="ime_container" class="prikaz_polje">Ime</div>
+        <div id="rodjendan_container" class="prikaz_polje">rodjen</div>
+        <div id="br_pasosa_container" class="prikaz_polje">br pasosa</div>
+        <div id="email_container" class="prikaz_polje">email</div>
+        <div id="telefon_container" class="prikaz_polje"></div>
+        <div id="od_container" class="prikaz_polje">od</div>
+        <div id="do_container" class="prikaz_polje">do</div>
+        <div id="br_dana_container" class="prikaz_polje">br dana</div>
+        <div id="tip_container" class="prikaz_polje">tip</div>
+        <!--Dugme za prikaz dodatnih osiguranika grupne polise-->
+        <div id="prikazi_container" class="prikazi_container">prikazi vise</div>
+    </div>  
+
     </div>
+    <!--Kraj korenog elementa i pocetak prozora za prikaz dodatnih osiguranika-->
+    <!--Ovde se sve popunjava dinamicki-->
     <div style="display: none;" id="do" class="dodatni_osiguranici">
     <h1 id="do_naslov" style="text-align:center;">Dodatni Osiguranici:</h1>
     <img src="../public/resursi/close.png" id="closeBtn" class="closeBtn">
@@ -46,9 +52,8 @@
    let userId =sessionStorage.getItem('id');
    console.log(userId);
    let polise = ""
-
    let polisa_arr =null;
-
+    //referenca na sve UI elemente
    let prijaveDiv = $('#prijave');
    let imeContainerDiv = $('#ime_container');
    let rodjendanContainerDiv = $('#rodjendan_container');
@@ -63,34 +68,24 @@
    let d_o=$('#do');
    let doc=$('#d_o_c');
    let closeBtn = $('#closeBtn');
-
-   
-
-
-
-   function calculateDateDifference(startDate, endDate) {
-    const startDateObj = new Date(startDate);
-    const endDateObj = new Date(endDate);
-
-    const differenceMs = endDateObj - startDateObj;
-
-    const differenceDays = differenceMs / (1000 * 60 * 60 * 24);
-
-    return differenceDays;
-}
+ 
+//funkcija za prikaz dodatnih osiguranika
 function prikaziDo(){
     d_o.css("display","flex");
 }
-    function prikaziPrijave(){
+//funkcija za prikaz svih elementa na prikazu prijave
+function prikaziPrijave(){
         $("#prijave").css("display", "flex");
+        //ukinuti onclick funkciju od prosle povezane polise
         prikaziContainerDiv.off('click');
-    }
-    function postaviPrijavu(polisa){
+}
+//postavi podatke input polise na odgovarajuca polja
+function postaviPrijavu(polisa){
         imeContainerDiv.text("Ime Nosioca: " + polisa.getPolisaIme())
         rodjendanContainerDiv.text("Datum Rodjenja: " + polisa.getPolisaDatumRodjenja())
         brPasosaContainerDiv.text("Broj Pasosa: " + polisa.getPolisaBrPasosa())
         emailContainerDiv.text("Email: " + polisa.getPolisaEmail());
-        if(polisa.getPolisaBrTelefona()===""){
+        if(polisa.getPolisaBrTelefona()===0 || polisa.getPolisaBrTelefona()===""){
             telefonContainerDiv.text("Nije Naveden Broj Telefona")
         }else{
             telefonContainerDiv.text("Broj Telefona: " + polisa.getPolisaBrTelefona());
@@ -99,14 +94,19 @@ function prikaziDo(){
         doContainerDiv.text("Do: " + polisa.getPolisaDo())
         brDanaContainerDiv.text(calculateDateDifference(polisa.getPolisaOd(),polisa.getPolisaDo()) + " dana");
         tipContainerDiv.text("Tip osiguranja: " + polisa.getPolisaTip())
+        //ako je grupno osiguranje, prikazi dugme i postavi onClick listener
         if(polisa.getPolisaTip()==='grupno'){
             prikaziContainerDiv.css('display','block');
             prikaziContainerDiv.text("Prikazi Dodatne Osiguranike");
+            //onclicklistener
             prikaziContainerDiv.off('click').on('click',function(e){
                 console.log(polisa.getPolisaDodatniOsiguranici())
                 let osiguranici =createDodatniOsiguranikArray(polisa.getPolisaDodatniOsiguranici())
                 console.log(osiguranici)
+                //isprazni pre nego sto postavis nove osiguranike da se ne bi
+                //beskonacno appendovalo
                 doc.empty();
+                //konstuisi DOM element za svakog osiguranika ponaosob
                 osiguranici.forEach(osiguranik=>{
                     const toAppend = `<div class="osiguranik">
                                         <p>${osiguranik.getIme()}<p/>
@@ -120,49 +120,12 @@ function prikaziDo(){
 
             })
         }else{
+            //sakrij dugme za dodatne osiguranike ako je osiguranje individualno
             prikaziContainerDiv.css('display','none');
         }
     }
-    
-    function dohvatiPolise(){
-    return new Promise((resolve, reject)=>{
-        $.ajax({
-            url:'../api/dohvati_polise.php',
-            method:"GET",
-            data:{id_korisnika:userId},
-            success:function(response){
-                resolve(response)
-                console.log(response);
 
-                polise=JSON.parse(response);
-                
-                polisa_arr=createPoliseArray(polise);
-
-                polisa_arr.forEach(val=>{
-                    console.log(val);
-                })
-
-            }
-
-        })
-    });
-}
-    function createPoliseArray(data) {
-    const poliseArray = [];
-    data.forEach(entry => {
-        console.log(entry);
-        const polisa = new Polisa(
-           entry.id_korisnika,entry.polisa_br_pasosa,
-           entry.polisa_br_telefona,entry.polisa_datum_rodjenja,
-           entry.polisa_od,entry.polisa_do,entry.polisa_ime,
-           entry.polisa_tip,entry.polisa_email,entry.polisa_dodatni_osiguranici,
-           entry.datum_prijave
-
-        );
-        poliseArray.push(polisa);
-    });
-    return poliseArray;
-}
+//funkcija od stringa formata podatak, podattak, podatak | podatak, podatak, podatak |... pravi dodatne osiguranike
 function createDodatniOsiguranikArray(str) {
     console.log('Received string:', str);
     const osiguraniciArray = [];
@@ -176,17 +139,9 @@ function createDodatniOsiguranikArray(str) {
     }
     return osiguraniciArray;
 }
+    //racuna br dana izmedju pocetnog i krajnjeg datuma
 
-
- 
-    
-   
-   $(document).ready(function(){
-    let userId = sessionStorage.getItem('id');
-    let polise = "";
-    let polisa_arr = null;
-
-    function calculateDateDifference(startDate, endDate) {
+   function calculateDateDifference(startDate, endDate) {
         const startDateObj = new Date(startDate);
         const endDateObj = new Date(endDate);
         const differenceMs = endDateObj - startDateObj;
@@ -194,6 +149,17 @@ function createDodatniOsiguranikArray(str) {
         return differenceDays;
     }
 
+    /*****Kraj-Pomocne Funkcije*****/
+
+  
+    /****Dokument loadovan******/
+   $(document).ready(function(){
+    let userId = sessionStorage.getItem('id');
+    let polise = "";
+    let polisa_arr = null;
+
+ 
+    /******Asinhrona funkcija*******/
     function dohvatiPolise() {
         return new Promise((resolve, reject) => {
             $.ajax({
@@ -209,9 +175,7 @@ function createDodatniOsiguranikArray(str) {
             });
         });
     }
-
-
-
+    //pravi polise iz parsovanog jsona koji je vracen iz asinhrone funkcije dohvatiPolise()
     function createPoliseArray(data) {
         const poliseArray = [];
         data.forEach(entry => {
@@ -226,12 +190,14 @@ function createDodatniOsiguranikArray(str) {
         });
         return poliseArray;
     }
+    
 
-    $(document).ready(function () {
+   
         let tableBody = $('#tabela');
         let listaPrijava = $('#listaPrijava');
         let counter = 1;
 
+        //dohvati polise i dodaj datum svih polisa u listu uz counter
         dohvatiPolise(userId).then(response => {
             polisa_arr.forEach((polisa, index) => {
                 const datumZaPolisu = `<div class="polisa" data-index="${index}">${counter}) ${polisa.datumPrijave}</div>`;
@@ -239,11 +205,12 @@ function createDodatniOsiguranikArray(str) {
                 counter++;
             });
         });
-
+        //onclicklistener da se zatvori prozor dodatnih osiguranika
         closeBtn.on('click',function(e){
             d_o.toggle();
         })
 
+        //postavljanje onClick listenera na svaku polisu koja se appendovala
         listaPrijava.on('click', '.polisa', function (e) {
             const index = $(this).data('index');
             const clickedPolisa = polisa_arr[index];
@@ -252,10 +219,12 @@ function createDodatniOsiguranikArray(str) {
             postaviPrijavu(clickedPolisa);
             $('.polisa').removeClass('selected');
             $(this).addClass('selected');
-            
         });
-    });
-});
+    })
+
+
+
+
 </script>
 
 </body>
